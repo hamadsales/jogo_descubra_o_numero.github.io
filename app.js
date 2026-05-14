b90e535435302a74c5453b3dfe1d3290323e31dc
Hamad Sales <hamadsales@gmail.com>
2026-05-14 17:28:28 -0300

    "mudança para Single Page Application (SPA)"

--- app.js
+++ app.js
@@ -1,9 +1,19 @@
-function easy(){
-
+function easy() {
+    //window.location.href = 'main.html'        // Muda de página
+    let numero = 5//Math.floor(Math.random() * 10)
 }
-function normal(){
-
+function normal() {
+    let numero = 10//Math.floor(Math.random() * 100)
 }
-function hard(){
-    
+function hard() {
+    let numero = 15//Math.floor(Math.random() * 1000)
+}
+
+function verificarChute(){
+    let chute = Number(document.querySelector('input#chute').value)
+    if(chute == numero){
+        window.alert('Acertou')
+    }else{
+        window.alert('tente novamente')
+    }
 }
